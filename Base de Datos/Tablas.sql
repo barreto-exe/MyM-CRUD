03d45d88a34198523cfdf4991381f0a25820c5ee
Karen Morán <kamoran.19@est.ucab.edu.ve>
2021-07-24 13:43:34 -0400

    CREATE primeras tablas atómicas

--- Base de Datos/Tablas.sql
+++ Base de Datos/Tablas.sql
@@ -9,6 +9,8 @@ CREATE DOMAIN dom_cedula AS VARCHAR(10);       --- dom para todas las cédulas--
 
 CREATE DOMAIN dom_direccion AS VARCHAR(75);   --- dom para las direcciones ---
 
+CREATE DOMAIN dom_descripcion AS VARCHAR(75);   --- dom para las descripciones ---
+
 CREATE DOMAIN dom_telefono AS VARCHAR(12);   --- dom para los teléfonos ---
 
 CREATE DOMAIN dom_fecha AS DATE;             --- dom para todas las fechas ---
@@ -18,75 +20,149 @@ CREATE DOMAIN dom_monto AS REAL;             --- dom para todos los precios / su
 CREATE DOMAIN dom_email AS VARCHAR(50);             --- dom para todos los emails ---
 
 CREATE DOMAIN dom_clave AS VARCHAR(16);             --- dom para todos las contraseñas ---
+
+CREATE DOMAIN dom_cantidad AS INTEGER;             --- dom para las cantidades ---
  
 ---------------------- Tablas ----------------------
-CREATE TABLE "ciudades"(
- 	nom_ciudad  		dom_nombre  	NOT NULL,
- 	PRIMARY KEY (nom_ciudad) 
+CREATE TABLE servicios(
+ 	cod_servicio  		dom_codigo  		NOT NULL,
+	nombre_s			dom_nombre			NOT NULL,
+	descripcion_s		dom_descripcion		NOT NULL,	
+	tiene_reserva		BOOLEAN				NOT NULL,
+	tiempo_r			dom_cantidad,			 
+ 	PRIMARY KEY (cod_servicio) 
 );
 
-CREATE TABLE "franquicias"(
+CREATE TABLE reservas(
+	num_reserva      		dom_codigo  	NOT NULL,
+ 	fecha_emision  			dom_fecha  		NOT NULL,
+	abono					dom_monto		NOT NULL,
+	ced_cliente				dom_cedula		NOT NULL,
+ 	PRIMARY KEY (num_reserva)
+);
+
+CREATE TABLE registros(
+	num_ficha      		dom_codigo  	NOT NULL,
+ 	fecha_ent  			dom_fecha	  	NOT NULL,
+	fecha_sal_est		dom_fecha		NOT NULL,
+	fecha_sal_real		dom_fecha		NOT NULL,
+	nom_autorizado		dom_nombre		NOT NULL,
+	ced_autorizado		dom_cedula		NOT NULL,		
+	placa_vehiculo		dom_codigo		NOT NULL,
+	rif_franquicia		dom_rif			NOT NULL,
+	num_reserva			dom_codigo,
+ 	PRIMARY KEY (num_ficha)
+);
+
+CREATE TABLE franquicias(
  	rif_franquicia  	dom_rif  		NOT NULL,
  	nombre_f 	    	dom_nombre  	NOT NULL,
+ 	ciudad  			dom_nombre   	NOT NULL,
  	direccion_f     	dom_direccion 	NOT NULL,
- 	ced_encargado  		dom_cedula   	NOT NULL,
- 	nom_ciudad  		dom_nombre   	NOT NULL,
-	clave			dom_clave		NOT NULL,
- 	PRIMARY KEY (rif_franquicia), 
- 	FOREIGN KEY (nom_ciudad) REFERENCES "ciudades"
-  		ON DELETE RESTRICT
-  		ON UPDATE CASCADE 
+ 	supervisor  		dom_cedula   	NOT NULL,
+	fecha_incio_sup		dom_fecha		NOT NULL,
+	clave				dom_clave		NOT NULL,
+ 	PRIMARY KEY (rif_franquicia)
 );
 
-CREATE TABLE "personas"(
-	cedula_p      		dom_cedula  	NOT NULL,
- 	nombre_p  			dom_nombre  	NOT NULL,
- 	telefono_p  		dom_telefono	NOT NULL,
- 	tipo_p  				CHAR(1)   		NOT NULL,
- 	CONSTRAINT status_cliente
-  		CHECK  (tipo_p = 'E' OR     --- empleado ---
-      			tipo_p = 'C'),       --- cliente ---
- 	PRIMARY KEY (cedula_p) 
-);
-
-CREATE TABLE "empleados"(
- 	ced_empleado  		dom_cedula  	NOT NULL,
+CREATE TABLE empleados(
+	cedula_e      		dom_cedula  	NOT NULL,
+ 	nombre_e  			dom_nombre  	NOT NULL,
+ 	telefono_e  		dom_telefono	NOT NULL,
  	sueldo  	    	dom_monto		NOT NULL,
  	direccion_e     	dom_direccion 	NOT NULL,
-	es_encargado		BOOLEAN			NOT NULL,
- 	rif_franquicia 	    dom_rif,
- 	PRIMARY KEY (ced_empleado), 
- 	FOREIGN KEY (ced_empleado) REFERENCES "personas"
-  		ON DELETE CASCADE
-  		ON UPDATE CASCADE,
- 	FOREIGN KEY (rif_franquicia) REFERENCES "franquicias"
-  		ON DELETE CASCADE
-  		ON UPDATE CASCADE
+ 	rif_franquicia 	    dom_rif			NOT NULL,
+ 	PRIMARY KEY (cedula_e)
 );
 
-CREATE TABLE "encargados"(
- 	ced_encargado  	dom_cedula  	NOT NULL,
- 	fecha_incio 	dom_fecha  		NOT NULL,
- 	PRIMARY KEY (ced_encargado), 
- 	FOREIGN KEY (ced_encargado) REFERENCES "empleados"
-  		ON DELETE CASCADE
-  		ON UPDATE CASCADE
+CREATE TABLE clientes(
+	cedula_c      		dom_cedula  	NOT NULL,
+ 	nombre_c  			dom_nombre  	NOT NULL,
+ 	tlf_principal  		dom_telefono	NOT NULL,
+ 	tlf_alternativo 	dom_telefono  	NOT NULL,
+ 	email  	    		dom_email		NOT NULL,
+ 	PRIMARY KEY (cedula_c)
 );
 
-CREATE TABLE "clientes"(
- 	ced_cliente  	dom_cedula  	NOT NULL,
- 	telefono_alt 	dom_telefono  	NOT NULL,
- 	email  	    	dom_email		NOT NULL,
- 	PRIMARY KEY (ced_cliente), 
- 	FOREIGN KEY (ced_cliente) REFERENCES "personas"
-  		ON DELETE CASCADE
-  		ON UPDATE CASCADE
+CREATE TABLE vehiculos(
+	placa				dom_codigo		NOT NULL,
+	cod_vehiculo   		dom_codigo  	NOT NULL,
+ 	tipo_aceite			dom_nombre  	NOT NULL,
+	fecha_adq			dom_fecha		NOT NULL,
+	nom_mecanico		dom_nombre		NOT NULL,
+	tlf_mecanico		dom_telefono	NOT NULL,
+	ced_dueno			dom_cedula		NOT NULL,
+	nom_modelo			dom_nombre		NOT NULL,
+ 	PRIMARY KEY (placa)
+);
+
+CREATE TABLE modelos(
+	nom_modelo			dom_nombre		NOT NULL,
+	aceite_motor		dom_descripcion	NOT NULL,
+	aceite_caja			dom_descripcion	NOT NULL,
+	peso		   		dom_monto	  	NOT NULL,
+ 	num_puestos			dom_cantidad  	NOT NULL,
+	refrigerante		dom_nombre		NOT NULL,
+	gasolina			dom_nombre		NOT NULL,
+	cod_marca			dom_codigo		NOT NULL,
+	cod_tipo_vehiculo	dom_codigo		NOT NULL,
+ 	PRIMARY KEY (nom_modelo)
+);
+
+CREATE TABLE marcas(
+	cod_marca      		dom_codigo  	NOT NULL,
+ 	nom_marca  			dom_nombre  	NOT NULL,
+	descripcion_ma		dom_descripcion	NOT NULL,
+ 	PRIMARY KEY (cod_marca)
+);
+
+CREATE TABLE tipos_vehiculo(
+	cod_tipo_vehiculo	dom_codigo  	NOT NULL,
+	descripcion_v		dom_descripcion	NOT NULL,
+ 	PRIMARY KEY (cod_tipo_vehiculo)
+);
+
+CREATE TABLE bancos(
+	cod_banco      		dom_codigo  	NOT NULL,
+ 	nombre_b  			dom_nombre  	NOT NULL,
+ 	PRIMARY KEY (cod_banco)
+);
+
+CREATE TABLE modalidades_pago(
+	descripcion_m      		dom_descripcion  	NOT NULL,
+ 	PRIMARY KEY (descripcion_m)
+);
+
+CREATE TABLE lineas_suministro(
+	cod_linea      		dom_codigo  	NOT NULL,
+ 	nombre_l  			dom_nombre  	NOT NULL,
+ 	PRIMARY KEY (cod_linea)
+);
+
+CREATE TABLE fabricantes(
+	cod_fabricante     		dom_codigo  	NOT NULL,
+ 	nombre_f	  			dom_nombre  	NOT NULL,
+ 	PRIMARY KEY (cod_fabricante)
+);
+
+CREATE TABLE proveedores(
+	rif_proveedor  			dom_rif  		NOT NULL,
+ 	razon_social  	    	dom_nombre		NOT NULL,
+	direccion_p				dom_direccion	NOT NULL,
+ 	tlf_local  				dom_telefono	NOT NULL,
+ 	tlf_celular		 		dom_telefono  	NOT NULL,
+ 	nom_contacto			dom_nombre  	NOT NULL,
+ 	PRIMARY KEY (rif_proveedor)
 );
 ---------------------- Claves Foráneas ----------------------
-ALTER TABLE "franquicias" 
-  ADD FOREIGN KEY ("ced_encargado") REFERENCES "encargados" ("ced_encargado") 
+ALTER TABLE franquicias 
+  ADD FOREIGN KEY (supervisor) REFERENCES empleados (cedula_e) 
 	ON DELETE RESTRICT 
 	ON UPDATE CASCADE;
 
+ALTER TABLE empleados 
+  ADD FOREIGN KEY (rif_franquicia) REFERENCES franquicias (rif_franquicia) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE;
+
 ---------------------- Índices ----------------------
-CREATE INDEX localizacion ON "franquicias" (nom_ciudad);
