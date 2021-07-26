371454df15b6c2da845e6813c7829a2bfd10fb90
Luis Barreto <luis.barreto.marin@gmail.com>
2021-07-26 01:37:21 -0400

    CRUD vehículos
    Sin probar

---
+++ MyM-CRUD/Model/Vehicle.cs
@@ -0,0 +1,107 @@
+﻿using MyM_CRUD.DataBase;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace MyM_CRUD.Model
+{
+    public class Vehicle : CrudObject<Vehicle>
+    {
+        public string Id { get; set; }
+        public string OilType { get; set; }
+        public DateTime DateAcquired { get; set; }
+        public string MechanicName { get; set; }
+        public string MechanicPhone { get; set; }
+        public string ModelName { get; set; }
+        public string OwnerId { get; set; }
+
+        public static List<Vehicle> SearchVehicles(string search)
+        {
+            //Traer datos de la BD
+            string query =
+                "SELECT * " +
+                "FROM vehicles  " +
+                "WHERE " +
+                "placa LIKE @Search ";
+            PostgreOp op = new PostgreOp(query);
+            op.PasarParametros("Search", $"%{search}%");
+
+            //Colocar resultados en memoria
+            DataTable result = QueryFromDataBase(op);
+
+            //Pasar datos a una lista
+            IEnumerable<Vehicle> v =
+                from DataRow dr in result.Rows
+                select new Vehicle()
+                {
+                    Id = dr["placa"].ToString(),
+                    OilType = dr["tipo_aceite"].ToString(),
+                    DateAcquired = (DateTime)dr["fecha_adq"],
+                    MechanicName = dr["nom_mecanico"].ToString(),
+                    MechanicPhone = dr["tlf_mecanico"].ToString(),
+                    OwnerId = dr["ced_dueno"].ToString(),
+                    ModelName = dr["nom_modelo"].ToString(),
+                };
+
+            return v.ToList();
+        }
+
+        public override void InsertTupleDatabase()
+        {
+            string query =
+                "INSERT " +
+                "INTO vehiculos (placa, tipo_aceite, fecha_adq, nom_mecanico, tlf_mecanico, ced_dueno, nom_modelo) " +
+                "VALUES (@placa, @tipo_aceite, @fecha_adq, @nom_mecanico, @tlf_mecanico, @ced_dueno, @nom_modelo)";
+            PostgreOp op = new PostgreOp(query);
+            op.PasarParametros("placa", Id);
+            op.PasarParametros("tipo_aceite", OilType);
+            op.PasarParametros("fecha_adq", DateAcquired);
+            op.PasarParametros("nom_mecanico", MechanicName);
+            op.PasarParametros("tlf_mecanico", MechanicPhone);
+            op.PasarParametros("ced_dueno", OwnerId);
+            op.PasarParametros("nom_modelo", ModelName);
+            op.EjecutarComando();
+        }
+
+        public override void UpdateTupleDataBase()
+        {
+            string query =
+                "UPDATE vehiculos " +
+                "SET tipo_aceite = @tipo_aceite," +
+                "fecha_adq = @fecha_adq," +
+                "nom_mecanico = @nom_mecanico," +
+                "tlf_mecanico = @tlf_mecanico," +
+                "ced_dueno = @ced_dueno, " +
+                "nom_modelo =  @nom_modelo" +
+                "WHERE placa = @placa";
+            PostgreOp op = new PostgreOp(query);
+            op.PasarParametros("placa", Id);
+            op.PasarParametros("tipo_aceite", OilType);
+            op.PasarParametros("fecha_adq", DateAcquired);
+            op.PasarParametros("nom_mecanico", MechanicName);
+            op.PasarParametros("tlf_mecanico", MechanicPhone);
+            op.PasarParametros("ced_dueno", OwnerId);
+            op.PasarParametros("nom_modelo", ModelName);
+            op.EjecutarComando();
+        }
+
+        protected override void BuildCrudObject(NpgsqlDataReader dr)
+        {
+            Id = dr["placa"].ToString();
+            OilType = dr["tipo_aceite"].ToString();
+            DateAcquired = (DateTime)dr["fecha_adq"];
+            MechanicName = dr["nom_mecanico"].ToString();
+            MechanicPhone = dr["tlf_mecanico"].ToString();
+            OwnerId = dr["ced_dueno"].ToString();
+            ModelName = dr["nom_modelo"].ToString();
+        }
+
+        protected override PostgreOp GetObjectOp(object[] keys)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyM-CRUD/View/Notebook/PageVehicles.xaml.cs
+++ MyM-CRUD/View/Notebook/PageVehicles.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using MyM_CRUD.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,17 +13,144 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using static MyM_CRUD.View.ICrudPage<MyM_CRUD.Model.Vehicle>;
 
 namespace MyM_CRUD.View
 {
     /// <summary>
     /// Lógica de interacción para PageVehicles.xaml
     /// </summary>
-    public partial class PageVehicles : Page
+    public partial class PageVehicles : Page, ICrudPage<Vehicle>
     {
+        private List<Vehicle> vehicles;
+        public State CurrentState { get; set; }
+
+
         public PageVehicles()
         {
             InitializeComponent();
+            SetReading();
+
+            //Eventos
+            ICrudPage<Vehicle> page = this;
+            TxtSearch.TextChanged += page.TxtSearch_TextChanged;
+            BtnEditSave.Click += page.BtnEditSave_Click;
+            Datagrid.SelectionChanged += page.Datagrid_SelectionChanged;
+            BtnAdd.Click += page.BtnAdd_Click;
+
+            //Inicializar datagrid
+            vehicles = Vehicle.SearchVehicles("");
+            Datagrid.ItemsSource = vehicles;
+        }
+
+
+        public void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            vehicles = Vehicle.SearchVehicles(TxtSearch.Text);
+            Datagrid.ItemsSource = vehicles;
+        }
+        public void Datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Vehicle vehicle = (Vehicle)Datagrid.SelectedItem;
+            LoadFields(vehicle);
+            SetReading();
+        }
+        public void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            LoadFields(null);
+            SetCreating();
+            TxtId.Focus();
+        }
+
+
+        public void LoadFields(Vehicle selected)
+        {
+            if(selected == null)
+            {
+                TxtId.Text = "";
+                TxtOilType.Text = "";
+                TxtDateAcquired.SelectedDate = null;
+                TxtMechanicName.Text = "";
+                TxtMechanicPhone.Text = "";
+                TxtModelName.Text = "";
+                TxtOwnerId.Text = "";
+                return;
+            }
+
+            TxtId.Text = selected.Id;
+            TxtOilType.Text = selected.OilType;
+            TxtDateAcquired.SelectedDate = selected.DateAcquired;
+            TxtMechanicName.Text = selected.MechanicName;
+            TxtMechanicPhone.Text = selected.MechanicPhone;
+            TxtModelName.Text = selected.ModelName;
+            TxtOwnerId.Text = selected.OwnerId;
+        }
+        public Vehicle GetObjectsFromFields() => new Vehicle
+        {
+            Id = TxtId.Text,
+            OilType = TxtOilType.Text,
+            DateAcquired = (DateTime)TxtDateAcquired.SelectedDate,
+            MechanicName = TxtMechanicName.Text,
+            MechanicPhone = TxtMechanicPhone.Text,
+            ModelName = TxtModelName.Text,
+            OwnerId = TxtOwnerId.Text,
+        };
+
+
+        public void SetCreating()
+        {
+            CurrentState = State.Creating;
+
+            TxtId.IsEnabled = true;
+            TxtOilType.IsEnabled = true;
+            TxtDateAcquired.IsEnabled = true;
+            TxtMechanicName.IsEnabled = true;
+            TxtMechanicPhone.IsEnabled = true;
+            TxtModelName.IsEnabled = true;
+            TxtOwnerId.IsEnabled = true;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.ContentSave;
+        }
+        public void SetReading()
+        {
+            CurrentState = State.Reading;
+
+            TxtId.IsEnabled = false;
+            TxtOilType.IsEnabled = false;
+            TxtDateAcquired.IsEnabled = false;
+            TxtMechanicName.IsEnabled = false;
+            TxtMechanicPhone.IsEnabled = false;
+            TxtModelName.IsEnabled = false;
+            TxtOwnerId.IsEnabled = false;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.Edit;
+        }
+        public void SetUpdating()
+        {
+            CurrentState = State.Updating;
+
+            TxtId.IsEnabled = true;
+            TxtOilType.IsEnabled = true;
+            TxtDateAcquired.IsEnabled = true;
+            TxtMechanicName.IsEnabled = true;
+            TxtMechanicPhone.IsEnabled = true;
+            TxtModelName.IsEnabled = true;
+            TxtOwnerId.IsEnabled = true;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.ContentSave;
+        }
+        public void BeginUpdating()
+        {
+            Vehicle selected = (Vehicle)Datagrid.SelectedItem;
+
+            if (selected == null) return;
+
+            LoadFields(selected);
+            SetUpdating();
         }
     }
 }
