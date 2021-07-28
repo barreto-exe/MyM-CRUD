44616d6ae33237465e965186da053e50fc3f983f
Luis Barreto <luis.barreto.marin@gmail.com>
2021-07-27 22:46:54 -0400

    Listado de productos
    Se pueden visualizar los productos (con algunos errores). Falta el método update

--- MyM-CRUD/Model/Product.cs
+++ MyM-CRUD/Model/Product.cs
@@ -12,18 +12,24 @@ namespace MyM_CRUD.Model
     {
         public string Code { get; set; }
         public string Name { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set; }
         public string ManufacturerCode { get; set; }
+        public ProductType Type { get; set; }
+        public bool? IsEcologic { get; set; }
+        public string? LineCode { get; set; }
 
         public static List<Product> SearchProducts(string search)
         {
             //Traer datos de la BD
             string query =
-                "";
-
+                "SELECT p.*, s.es_ecologico, s.cod_linea " +
+                "FROM productos p " +
+                "LEFT JOIN servicio_productos s ON (p.cod_producto = s.cod_producto) " +
+                "WHERE p.nombre_p LIKE @Search " +
+                "OR p.cod_producto LIKE @Search";
             PostgreOp op = new PostgreOp(query);
             op.PasarParametros("Search", $"%{search}%");
-            op.PasarParametros("Rif", App.Session.Branch);
 
             //Colocar resultados en memoria
             DataTable result = QueryFromDataBase(op);
@@ -31,28 +37,77 @@ namespace MyM_CRUD.Model
             //Pasar datos a una lista
             IEnumerable<Product> products =
                 from DataRow dr in result.Rows
-                select new Product()
-                {
-                };
+                select BuildProductFromDr(dr);
 
             return products.ToList();
         }
+        private static Product BuildProductFromDr(dynamic dr)
+        {
+            Product product = new Product()
+            {
+                Code = dr["cod_producto"].ToString(),
+                Name = dr["nombre_p"].ToString(),
+                Price = Tools.Tools.Object2Decimal(dr["precio_p"]),
+                Description = dr["descripcion_p"].ToString(),
+                ManufacturerCode = dr["cod_fabricante"].ToString(),
+                Type = (ProductType) dr["tipo_p"].ToString()[0],
+            };
+
+            if(product.Type == ProductType.ForService)
+            {
+                product.IsEcologic = (bool)dr["es_ecologico"];
+                product.LineCode = dr["cod_linea"].ToString();
+            }
+
+            return product;
+        }
+
 
         public override void InsertTupleDatabase()
         {
-            throw new NotImplementedException();
+            string query =
+                "INSERT INTO productos(cod_producto, precio_p, nombre_p, descripcion_p, tipo_p, cod_fabricante) " +
+                "VALUES (@cod_producto, @precio_p, @nombre_p, @descripcion_p, @tipo_p, @cod_fabricante); ";
+            PostgreOp op = new PostgreOp(query);
+            op.PasarParametros("cod_producto", Code);
+            op.PasarParametros("precio_p", Price);
+            op.PasarParametros("nombre_p", Name);
+            op.PasarParametros("descripcion_p", Description);
+            op.PasarParametros("tipo_p", (char)Type);
+            op.PasarParametros("cod_fabricante", ManufacturerCode);
+
+            if (Type == ProductType.ForSell)
+            {
+                op.Query += 
+                    "INSERT INTO tienda_productos " +
+                    "VALUES (@cod_producto); ";
+            }
+            if(Type == ProductType.ForService)
+            {
+                op.Query +=
+                    "INSERT INTO servicio_productos(cod_producto, es_ecologico, cod_linea) \n" +
+                    "VALUES (@cod_producto, @es_ecologico, @cod_linea); ";
+                op.PasarParametros("es_ecologico", IsEcologic);
+                op.PasarParametros("cod_linea", LineCode);
+            }
+
+            op.EjecutarComando();
         }
         public override void UpdateTupleDataBase()
         {
             throw new NotImplementedException();
         }
-        protected override void BuildCrudObject(NpgsqlDataReader dr)
-        {
-            throw new NotImplementedException();
-        }
+        protected override void BuildCrudObject(NpgsqlDataReader dr) => BuildProductFromDr(dr);
+
         protected override PostgreOp GetObjectOp(object[] keys)
         {
             throw new NotImplementedException();
+        }
+
+        public enum ProductType
+        {
+            ForService = 'S',
+            ForSell = 'T',
         }
     }
 }
--- MyM-CRUD/View/Inventory/PageProducts.xaml.cs
+++ MyM-CRUD/View/Inventory/PageProducts.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using MyM_CRUD.Model;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,17 +14,159 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MyM_CRUD.Model.Product;
+using static MyM_CRUD.View.ICrudPage<MyM_CRUD.Model.Product>;
 
 namespace MyM_CRUD.View
 {
     /// <summary>
     /// Lógica de interacción para PageProducts.xaml
     /// </summary>
-    public partial class PageProducts : Page
+    public partial class PageProducts : Page, ICrudPage<Product>
     {
+        private List<Product> products;
+        public State CurrentState { get; set; }
+
         public PageProducts()
         {
             InitializeComponent();
+            SetReading();
+
+            //Eventos
+            ICrudPage<Product> page = this;
+            TxtSearch.TextChanged += page.TxtSearch_TextChanged;
+            BtnEditSave.Click += page.BtnEditSave_Click;
+            Datagrid.SelectionChanged += page.Datagrid_SelectionChanged;
+            BtnAdd.Click += page.BtnAdd_Click;
+
+            //Inicializar datagrid
+            products = Product.SearchProducts("");
+            Datagrid.ItemsSource = products;
+        }
+
+        public void TxtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            products = Product.SearchProducts(TxtSearch.Text);
+            Datagrid.ItemsSource = products;
+        }
+        public void Datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Product product = (Product)Datagrid.SelectedItem;
+            LoadFields(product);
+            SetReading();
+        }
+
+        public void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            LoadFields(null);
+            SetCreating();
+            TxtCode.Focus();
+        }
+
+
+        public Product GetObjectsFromFields() => new Product()
+        {
+            Code = TxtCode.Text,
+            Name = TxtName.Text,
+            Price = Convert.ToDecimal(TxtPrice.Text),
+            Description = TxtDescription.Text,
+            ManufacturerCode = TxtManufacturerCode.Text,
+            Type = CbTypeProduct.SelectedIndex == 1 ? ProductType.ForService : ProductType.ForSell,
+            IsEcologic = CheckIsEcologic.IsChecked,
+            LineCode = TxtLineCode.Text,
+        };
+
+        public void LoadFields(Product selected)
+        {
+            if(selected == null)
+            {
+                TxtCode.Text = "";
+                TxtName.Text = "";
+                TxtPrice.Text = "";
+                TxtDescription.Text = "";
+                TxtManufacturerCode.Text = "";
+                CbTypeProduct.SelectedIndex = -1;
+                CheckIsEcologic.IsChecked = false;
+                TxtLineCode.Text = "";
+            }
+
+            TxtCode.Text = selected.Code;
+            TxtName.Text = selected.Name;
+            TxtPrice.Text = selected.Price.ToString();
+            TxtDescription.Text = selected.Description;
+            TxtManufacturerCode.Text = selected.ManufacturerCode;
+            switch(selected.Type)
+            {
+                case ProductType.ForSell:
+                    {
+                        StackEcologic.Visibility = Visibility.Collapsed;
+                        break;
+                    }
+                case ProductType.ForService:
+                    {
+                        StackEcologic.Visibility = Visibility.Visible;
+
+                        CheckIsEcologic.IsChecked = selected.IsEcologic;
+                        TxtLineCode.Text = selected.LineCode;
+                        break;
+                    }
+            }
+        }
+
+
+        public void SetCreating()
+        {
+            CurrentState = State.Creating;
+
+            TxtCode.IsEnabled = true;
+            TxtName.IsEnabled = true;
+            TxtPrice.IsEnabled = true;
+            TxtDescription.IsEnabled = true;
+            TxtManufacturerCode.IsEnabled = true;
+            CheckIsEcologic.IsEnabled = true;
+            CbTypeProduct.IsEnabled = true;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.ContentSave;
+        }
+        public void SetReading()
+        {
+            CurrentState = State.Reading;
+
+            TxtCode.IsEnabled = true;
+            TxtName.IsEnabled = true;
+            TxtPrice.IsEnabled = true;
+            TxtDescription.IsEnabled = true;
+            TxtManufacturerCode.IsEnabled = true;
+            CheckIsEcologic.IsEnabled = true;
+            CbTypeProduct.IsEnabled = true;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.Edit;
+        }
+        public void SetUpdating()
+        {
+            CurrentState = State.Updating;
+
+            TxtCode.IsEnabled = true;
+            TxtName.IsEnabled = true;
+            TxtPrice.IsEnabled = true;
+            TxtDescription.IsEnabled = true;
+            TxtManufacturerCode.IsEnabled = true;
+            CheckIsEcologic.IsEnabled = true;
+            CbTypeProduct.IsEnabled = true;
+
+            BtnEditSave.Visibility = Visibility.Visible;
+            IconEdit.Kind = PackIconKind.ContentSave;
+        }
+        public void BeginUpdating()
+        {
+            Product selected = (Product)Datagrid.SelectedItem;
+
+            if (selected == null) return;
+
+            LoadFields(selected);
+            SetUpdating();
         }
     }
 }
