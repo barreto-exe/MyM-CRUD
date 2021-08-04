ebe7fe2fcae11d63484f85466ae2c7a71ec855db
María Guerra <mariafgg53@gmail.com>
2021-08-04 19:57:35 -0400

    Graficas para estadisticas

--- MyM-CRUD/View/Stadistics/PageEstadisticas.xaml.cs
+++ MyM-CRUD/View/Stadistics/PageEstadisticas.xaml.cs
@@ -25,23 +25,69 @@ namespace MyM_CRUD.View
         {
             InitializeComponent();
 
+            LiveCharts.Wpf.Separator sep = new LiveCharts.Wpf.Separator();
+            Axis axis = new Axis();
+            sep.IsEnabled = false;
+            sep.Step = 5;
+            axis.Separator = sep;
+            cartesianChart2.AxisX.Add(axis);
+            cartesianChart4.AxisX.Add(axis);
+            cartesianChart1.AxisX.Add(axis);
+            cartesianChart3.AxisX.Add(axis);
+            cartesianChart2.AxisY.Add(axis);
+            cartesianChart4.AxisY.Add(axis);
+            cartesianChart1.AxisY.Add(axis);
+            cartesianChart3.AxisY.Add(axis);
+
+            cartesianChart2.Series = new SeriesCollection()
+            {
+                new RowSeries(){
+                    Values = new ChartValues<double> {25,15,20},
+                    Fill = Brushes.Green
+                    
+                },
+                
+                
+            };
+            
+            cartesianChart4.Series = new SeriesCollection()
+            {
+                new StackedAreaSeries(){
+                    Values = new ChartValues<double> {25,15,20},
+                    Fill = Brushes.GreenYellow
+                }
+            };
+            
             cartesianChart1.Series = new SeriesCollection()
             {
                 new LineSeries()
                 {
-                    Values = new ChartValues<decimal> { 5, 6, 2, 7 }
+                    Values = new ChartValues<decimal> { 5, 6, 2, 20 },
+
                 },
                 new LineSeries()
                 {
                     Values = new ChartValues<double> { 3, 5, 7, 4 }
                 }
+
+            };
+
+            cartesianChart3.Series = new SeriesCollection()
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<decimal> { 8, 10, 2, 25 },
+                    Fill = Brushes.LightYellow
+                },
                 
+
             };
         }
 
         private void cartesianChart1_Loaded(object sender, RoutedEventArgs e)
         {
-
+            
         }
+
     }
 }
